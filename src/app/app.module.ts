95a1868507753c8b9a0ea1c321f2f81f42573ba0
Márcio Pulcinelli <mpulcinelli@gmail.com>
2018-09-11 21:15:10 -0300

    ajustes iniciais e configuração do menu.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule }  from "./app-routing.module";
 import { SobreComponent } from './sobre/sobre.component';
 
 import { DashboardModule }  from './dashboard/dashboard.module';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SobreComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserModule,
     HttpClientModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot(),
     DashboardModule
   ],
   providers: [],
